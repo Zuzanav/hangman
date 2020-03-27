9dbeb7b7f119abfd340a8bf14e82575e4fd09db3
Zuzanav <zuzanavolny@gmail.com>
2020-03-26 19:25:14 -0700

    onkeyup event added to store user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,43 +10,56 @@ let words = ["demogorgan", "upside down", "eleven", "hawkins", "spy", "waffles",
 "magnets", "hopper", "starcourt"];
 
 
-// VARIABLES FOR SCORES
+// variables for scores 
 let wins = 0;
 let losses = 0;
 let guesses = 6;
 let characterGuessed;
 
-// COMPUTER SELECTS RANDOM WORD --------------------------
-let randomWord = words[Math.floor(Math.random()*words.length)]
-console.log(randomWord);
+// array defined to hold the answer and display on DOM
+let answerArray = [];
 
 //-------------------------------------------------------------------------
 
 
-// FUNCTIONS  =====================================================================================
+// GAME SET UP  =====================================================================================
 
-// array defined to hold the answer and display on DOM
-let answerArray = [];
+// computer selects a random word from array
+let randomWord = words[Math.floor(Math.random()*words.length)]
+console.log(randomWord);
 
-    // loop creates x amount of underscores "_" based on amount of characters in selected word
-    for (let i=0; i<randomWord.length; i++){
-        answerArray[i] = "_"
-    };
+// SET UP BOARD -----------------------------------------------------------------------------
+// loop creates x amount of underscores "_" based on amount of characters in selected word
+for (let i=0; i<randomWord.length; i++){
+    answerArray[i] = "_"
+};
 
 // saving character count from selected word for comparison later...
 let wordCharCount = randomWord.length;
 
 console.log(answerArray);
 
-//write underscores to the DOM 
+//print underscores to the DOM 
 $(document).ready(function(){ 
     $("#randomWord").html(answerArray);
-})
+});
+// -----------------------------------------------------------------------------------------
 
 
+// EVENTS =====================================================================================
+
+// Watch for user to press a key
+document.onkeyup = function () {
+
+    // save pressed key
+    let letterGuessed = event.key;
+
+    console.log(letterGuessed);
+
+}
 
 
-
+// -----------------------------------------------------
 
 
 
