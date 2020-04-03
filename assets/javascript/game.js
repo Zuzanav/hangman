5714e5f8b0c252091a7f01fdc318a29116bcb41e
Zuzanav <zuzanavolny@gmail.com>
2020-04-03 13:24:43 -0700

    remove flicker on load

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,12 @@
 // =======================================================================================
+
+// LOAD WHEN ALL CSS IS READY
+document.write('<style type="text/css">body{display:none}</style>');
+jQuery(function($) {
+$('body').css('display','block');
+});
+
+
 // LOAD WHEN DOM IS READY ---------------------------------------------------
 $(document).ready(function() {
     
